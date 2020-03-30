f75a998daa8514d84daca0984cc383a9569ba2a6
Kel <kelstalker@hotmail.com>
2020-03-30 15:16:32 -0600

    Only 'include a class' and 'spread/rest parameter' criteria yet to be added

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
 
-const array1 = new Array (1, [2, 3]);
-const array2 = new Array (1, [2, [3, 4]]);
-const array3 = new Array ([1, [2, [3, [4, [5, 6]]]]]);
-const array4 = new Array ([1, [2], 1, [2], 1]);
-const array5 = new Array ("insects", "birds", "snakes");
-// output for these 4 arrays should be (in order): 3, 4, 6, 5
+// 5 arrays to test .flat method - expected output for whichever array is used: 3, 4, 6, 5, 3
+const array1 = new Array(1, [2, 3]);
+const array2 = new Array(1, [2, [3, 4]]);
+const array3 = new Array([1, [2, [3, [4, [5, 6]]]]]);
+const array4 = new Array([1, [2], 1, [2], 1]);
+const array5 = new Array("insects", "birds", "snakes");
 
-// NOTE - I have used the 'flat()' method which flattens an array to any 'depth'
-// I could (as an added feature later) create a function that counts the number
-// of 'inside-array-elements' (the largest would be the deepest), but for now
-// I'm using 'infinity' as per several online resources for this topic
-// Trying to push this
-
-console.log (array2.length);
-
-    function getLength(x) {
-        return x.flat(Infinity).length;
-    
-}
+// an array of numbers to use the 'map' method on.
+var numbers = [4, 9, 16, 25, 36, 49]
 
 
-
+// This example demonstrates arrays
+// No time to create html dropdown box with list of arrays, but will attempt before midnight
+console.log("Array length ", `"`, "Before", `"`, ".flat method invoked");
+console.log(array2.length);
+console.log("Array length ", `"`, "After", `"`, ".flat method invoked");
 console.log(array2.flat(Infinity).length);
-console.log ("This is my js script page, AND THIS TEXT SHOWS UP!");
+
+
+// Borrowed from w3schools.com - JavaScript Array map() Method
+console.log("Sqrt of numbers array using", `"`, ".map", `"`, "method");
+console.log(numbers.map(Math.sqrt));
+
+function getLength(x) {
+    return x.flat(Infinity).length;
+};
